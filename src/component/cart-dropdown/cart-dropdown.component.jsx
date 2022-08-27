af6b23646350d2d197fc7d1395e95083ca25f836
AlexKadai <alexandrkadai@gmail.com>
2022-08-28 00:45:29 +0300

    Cart context and functions are made

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,18 @@
-import './cart-dropdown.styles.scss';
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
+import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
+
+import './cart-dropdown.styles.scss';
 const CartDropdown = () =>{
+    const {cartItems} = useContext(CartContext);
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-             <Button>Go to Checkout</Button>
+                {cartItems.map((item) => <CartItem key={item.id} cartItem={item}/>)}
             </div>
+             <Button>Go to Checkout</Button>
         </div>
     )
 };
