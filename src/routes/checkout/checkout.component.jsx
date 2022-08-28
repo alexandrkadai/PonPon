49523a8f49ef24c1e0af3d41dfc637676efe6b1e
AlexKadai <alexandrkadai@gmail.com>
2022-08-28 22:06:05 +0300

    Checout started

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
+
+import './checkout.styles.scss';
+
+const Checout = () =>{
+    const { cartItems } = useContext(CartContext);
+    const { }
+    return (
+        <div>
+        <img/>
+        <span></span>
+        <span></span>
+        <span></span>
+        </div>
+    )
+}
+
+export default Checout;
