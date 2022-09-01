336a2b154a26e426f81f25929a2952ce5575207b
AlexKadai <alexandrkadai@gmail.com>
2022-09-01 03:19:17 +0300

    Completed

--- src/component/directory-item/directory-item.component.jsx
+++ src/component/directory-item/directory-item.component.jsx
@@ -1,9 +1,12 @@
+import {useNavigate} from 'react-router-dom';
 import {DirectoryItemContainer, Body, BackgroundImage} from './directory-item.style';
 
 const DirectoryItem = ( { category }) => {
-    const { title, url } = category ;
+    const { title, url} = category ;
+    const navigate = useNavigate();
+    const onNavigateHandler = () => navigate(`shop/` + title);
     return(
-        <DirectoryItemContainer >
+        <DirectoryItemContainer onClick={onNavigateHandler}>
         <BackgroundImage url={url} />
         
         <Body>
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -6,7 +6,8 @@ const Home = () => {
     {
       id : 1,
       title: 'hats',
-      url: 'https://media.istockphoto.com/id/1041773030/es/vector/conjunto-de-vector-plano-de-sombreros-de-hombre-y-mujer-elegantes-sombreros-masculinos-y.webp?s=2048x2048&w=is&k=20&c=EAKiLZ5RbdcbJmSWBYq73irB1kV9ryUeKvd9tUYdFgo='
+      url: 'https://media.istockphoto.com/id/1041773030/es/vector/conjunto-de-vector-plano-de-sombreros-de-hombre-y-mujer-elegantes-sombreros-masculinos-y.webp?s=2048x2048&w=is&k=20&c=EAKiLZ5RbdcbJmSWBYq73irB1kV9ryUeKvd9tUYdFgo=',
+      route: 'shop/hats'
     },
     {
       id : 2,
@@ -15,17 +16,17 @@ const Home = () => {
     },
     {
       id : 3,
-      title: 'shoes',
+      title: 'sneakers',
       url: 'https://media.istockphoto.com/id/974771930/es/vector/vector-set-de-calzado-zapatos-con-estilo-para-hombre-y-mujer-sandalias-diferentes-%C3%A9pocas-del.webp?s=612x612&w=is&k=20&c=OStf4XnsGGMN1QzJMB414Ru0a5yqmw5rT9VmkbC1oow='
     },
     {
       id : 4,
-      title: 'women',
+      title: 'womens',
       url: 'https://media.istockphoto.com/id/1296144988/es/vector/cinco-mujeres-de-diferentes-etnias-y-culturas-est%C3%A1n-juntas-una-al-lado-de-la-otra-las-chicas.webp?s=612x612&w=is&k=20&c=X5ko4gReeWrq5Hgo0iL1opfr9azRMbpXvrLLnf5Z_8A='
     },
     {
       id : 5,
-      title: 'men',
+      title: 'mens',
       url: 'https://media.istockphoto.com/id/492529287/es/foto/retrato-de-feliz-sonriente-hombre.webp?s=612x612&w=is&k=20&c=1Re5YIZQqy800E9H6jVue0qv3TEvJXLYzmv-QncUbBo='
     },
   ];
