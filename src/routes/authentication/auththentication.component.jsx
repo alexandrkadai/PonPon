94680e427fb02067006c85d6e6fc86d238379357
alex kadai <alexandrkadai@gmail.com>
2022-09-10 14:55:07 +0300

    Category Redux

--- src/routes/authentication/auththentication.component.jsx
+++ src/routes/authentication/auththentication.component.jsx
@@ -1,4 +1,3 @@
-
 import SignUpForm  from '../../component/sign-up/sign-up-form.component';
 import SignInForm from '../../component/sign-in-form/sign-in-form.component';
 import {AuthenticationContainerDiv} from './authentication.styles';
