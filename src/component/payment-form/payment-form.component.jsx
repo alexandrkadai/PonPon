ce13d364cd21159be2267728d071006cf50695d6
alex kadai <alexandrkadai@gmail.com>
2022-09-20 17:27:11 +0300

    Payment Complete

--- src/component/payment-form/payment-form.component.jsx
+++ src/component/payment-form/payment-form.component.jsx
@@ -5,16 +5,48 @@ import { PaymentFormContainer, FormContainer } from './payment-form.styles';
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+
   const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+    
+    console.log(response);
+    
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+    console.log(client_secret);
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Alex Kadai',
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successful');
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>CreditCard Payment</h2>
         <CardElement />
         <Button buttonType={Button_Types_Classes.inverted}>Complete Payment</Button>
